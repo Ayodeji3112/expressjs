234f329d010baf3bfe4812f0e3aefd7de468d986
Ayodeji3112 <154736913+Ayodeji3112@users.noreply.github.com>
2024-03-28 20:22:24 +0000

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const { clog } = require('./middleware/clog');
 const api = require('./routes/index.js');
 
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 300;
 
 const app = express();
 
